a7e1e41828c72a91619df79e08fbb783ad596b43
Goudet Ethan <ethangoudet@gmail.com>
2025-02-03 11:53:58 +0100

    correction home

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,5 +1,5 @@
 import {Routes, Route} from "react-router";
-import Home from "../pages/HOme";
+import Home from "../components/Home/home";
 import FetchHeros from "../components/FetchHeors/fetchHeros";
 import { AuthProvider } from "../auth/AuthProvider";
 import PrivateRoute from "../auth/PrivateRoute";
